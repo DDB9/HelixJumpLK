82635ba6fab7a6f00d4d296779d026a40cbfc3b2
Daan de Bruijn <ddebruijn9@gmail.com>
2024-08-22 14:47:30 +0200

    Revert "[+] Added Hourglass Powerup that slows down time."
    This reverts commit 33917ee5a54538b0319987d913aa7ba347fa3476.

--- HelixJump/Assets/_scripts/Hourglass.cs
+++ HelixJump/Assets/_scripts/Hourglass.cs
@@ -4,18 +4,10 @@ using UnityEngine;
 
 public class Hourglass : Powerup
 {
-    public float TimeScale;
-
     public override IEnumerator ActivatePowerup()
     {
-        if (!GameManager.Instance.ActivePowerups.Contains(this)) GameManager.Instance.ActivePowerups.Add(this);
-
-        transform.GetChild(0).gameObject.SetActive(false);
-
-        Time.timeScale = TimeScale;
-        yield return new WaitForSeconds(TimeScale / PowerupDuration + 1);
+        Time.timeScale = 0.5f;
+        yield return new WaitForSeconds(PowerupDutaion);
         Time.timeScale = 1.0f;
-
-        DeactivatePowerup();
     }
 }
--- HelixJump/Assets/_scripts/Powerup.cs
+++ HelixJump/Assets/_scripts/Powerup.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 
 public abstract class Powerup : MonoBehaviour
 {
-    public float PowerupDuration;
+    public float PowerupDutaion;
     public abstract IEnumerator ActivatePowerup();
-
-    private void Update()
-    {
-        transform.LookAt(Camera.main.transform.position);
-    }
-
-    public void ResetPowerup()
-    {
-        gameObject.SetActive(true);
-        transform.GetChild(0).gameObject.SetActive(true);
-    }
-
-    public void DeactivatePowerup()
-    {
-        gameObject.SetActive(false);
-    }
 }
